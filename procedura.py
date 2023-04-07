b1b82d0e2b81442be0a411045df761ebaa8ebbe9
JakubCacek <79901378+JakubCacek@users.noreply.github.com>
2023-04-07 13:23:46 +0200

    Update procedura.py

--- procedura.py
+++ procedura.py
@@ -1,44 +1,39 @@
-#działa
-# Lista = []
-# list_num = 0
-# trials = 13
-# for i in range(trials):
-#     list_num = list_num + 1
-#     Lista.append(list_num)
-    
-# print(Lista)
-
 import random
+
 
 Lista_zadan = ["Num", "Let"] # 2 rodzaje zadań, między którymi badani mają się przełączać
 Let_stim = ["a", "b", "c"] #Zadanie z liter
 Num_stim = [1,2,3,4,5] #zadanie z liczb 
 trial = 0 #numer trialu
 trial_num = [] #lista do zapisu numeru trialu
+task_hist = [] #poprzedni trial
 trial_leng = 10 #testowa liczba triali
 
 for i in range(trial_leng):
     trial = trial + 1
     trial_num.append(trial)
-    if trial == 1: #w pierwszym trialu badany losouje jedno z zadań
+    if trial == 1:
         task_type = random.choice(Lista_zadan)
+        task_hist.append(task_type) #zapis poprzedniego trialu
         if task_type == "Let":
             print(random.choice(Let_stim))
         elif task_type == "Num":
             print(random.choice(Num_stim))
     else:
-        rn = random.random() # po pierwszym trialu losowany jest numer. Jeśli jest on mniejszy lub równy 0.25, to powinno wyświetlić im się inne zadanie
-        if rn <= 0.25:
-            Lista_zadan[i] != task_type[i-1] #wydawało mi się, że przełączenia można zrobić tak, ale pokazuje IndexError: list index out of range
+        rn = random.random()
+        if rn >= 0.25:
+            Lista_zadan[i] != task_hist[i-1]
             task_type = Lista_zadan
+            task_hist.append(task_type)
                 #save trial_type
             if task_type == "Let":
                 print(random.choice(Let_stim))
             elif task_type == "Num":
                 print(random.choice(Num_stim))
-        elif rn > 0.25:
-            Lista_zadan[i] == task_type[i-1] #jeśli wyszło więcej niż 25, to powinno być to samo zadanie co ostatnio
+        elif rn < 0.25:
+            Lista_zadan[i] == task_hist[i-1]
             task_type = Lista_zadan
+            task_hist.append(task_type)
             if task_type == "Let":
                 print(random.choice(Let_stim))
             elif task_type == "Num":
